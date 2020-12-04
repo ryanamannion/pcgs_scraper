9d8df035f42a7d5536fa927113e5fa7ddb14ae15
ryanamannion <ram321@georgetown.edu>
2020-12-03 22:56:42 -0500

    fixes mixed tabs and spaces

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 import pcgs_scraper
 
 with open("README.md", "r") as f:
-	long_description = f.read()
+    long_description = f.read()
 
 setuptools.setup(
-	name="pcgs_scraper",
-	version=pcgs_scraper.__version__,
-	author="Ryan A. Mannion",
-	author_email="ram321@georgetown.edu",
+    name="pcgs_scraper",
+    version=pcgs_scraper.__version__,
+    author="Ryan A. Mannion",
+    author_email="ram321@georgetown.edu",
     description="tools for scraping coin data from pcgs",
     long_description=long_description,
     long_description_content_type="text/markdown",
