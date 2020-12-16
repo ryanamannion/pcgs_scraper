207d79fb3417dc728e031d4eff041727732e1461
Ryan A. Mannion <ryanamannion@users.noreply.github.com>
2020-12-16 14:21:11 -0500

    removes requirements.txt

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ import pcgs_scraper
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().split('\n')
-
 setuptools.setup(
     name="pcgs_scraper",
     version=pcgs_scraper.__version__,
@@ -17,5 +14,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=['pcgs_scraper'],
     python_requires='>=3.7',
-    install_requires=requirements
+    install_requires=[
+        'beautifulsoup4',
+        'ft',
+        'requests'
+    ]
 )
